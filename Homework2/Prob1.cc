4da8effd73d9ee9221ab34fb3d4de878caf61cbd
NJerla <ncjerla@buffalo.edu>
2017-02-20 16:50:10 -0500

    Adding updated Prob1.cc

--- Homework2/Prob1.cc
+++ Homework2/Prob1.cc
@@ -21,7 +21,7 @@ int main(void)
   std::cin >> x2;
   std::cin >> y2;
 
-    if(std::abs(x1-x2) < 0.000001)
+ if(std::abs(x1-x2) < 0.000001)
       {
 	midpointXval = (x1 + x2)/2;
 	midpointYval = (y1 + y2)/2;
@@ -29,18 +29,25 @@ int main(void)
 	std::cout << "The midpoint of your two points are (x,y) = " << "(" << midpointXval << "," << midpointYval  << ")" << " but you were naughty and divided "
 	" by zero. Your slope is infinite with no y-intercept!! The equation of your line is x = " << x1 << " with a line perpendicular to the first point being y = " << y1 << std::endl;
       }
-   else
+ else
       {
 	midpointXval = (x1 + x2)/2;
 	midpointYval = (y1 + y2)/2;
 	slope = (y2 - y1)/(x2 - x1);
 	yintercept = (y1 - slope*x1);
-	perpslope = (-1)* ((x2 - x1)/(y2 - y1));
-	perpyintercept = (y1 - perpslope*x1);
 	std::cout << "Your two points were (x1,y1) = " << "(" << x1 << "," << y1 << ") and (x2,y2) = " << "(" << x2 << "," << y2 << ")."  << std::endl;
 	std::cout << "The midpoint of your two points are (x,y) = " << "(" << midpointXval << "," << midpointYval  << ")" << " with a slope of m = " << slope << " and a y-intercept of b = "
-	   << yintercept << "." << " The equation of your line is y = " << slope << "x + " << yintercept << ". Equation of line perpendicular to first point entered is y = "
-	   << perpslope << "x + " << perpyintercept << "." <<  std::endl;
+		  << yintercept << "." << " The equation of your line is y = " << slope << "x + " << yintercept << std::endl;
+	if(std::abs(y1-y2) < 0.000001)
+	  {
+	    std::cout << "The line perpendicular to your origional one has a infinite slope, Naughty!!! The eqation of the line passing through your first point is of the form x = " << y1 << std::endl;
+	  }
+	else
+	  {
+	    perpslope = (-1)* ((x2 - x1)/(y2 - y1));
+	    perpyintercept = (y1 - perpslope*x1);
+	    std::cout << " Equation of line perpendicular to first point entered is y = " << perpslope << "x + " << perpyintercept << "." <<  std::endl;
+	  }
       }
  
   return 0;
