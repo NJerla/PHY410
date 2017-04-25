477ad2caf3b091da2f493816fedbdef267fd2e4d
NJerla <ncjerla@buffalo.edu>
2017-04-24 21:50:53 -0400

    Adding updated quadrature.py

--- Lecture10/quadrature.py
+++ Lecture10/quadrature.py
@@ -5,15 +5,18 @@ from math import *
 
 
 n1 = 1
-n2 = 0
+n2 = 201
 
 while n1 % 2 != 0 :
     n1 = int(raw_input( "Enter number of intervals desired for trapezoidal rule (must be even)" ))
 
-a = 0
-b = 2 * atan(1.0)
-ans1 = trapezoid(sin, a, b, n1)
+a = 0.1
+b = 1.0
+ans1 = trapezoid(exp, a, b, n1)
 print 'Trapezoidal rule = ' + str(ans1)
 
-ans2 = adaptive_trapezoid(sin, a, b, 0.0001)
+ans2 = adaptive_trapezoid(exp, a, b, 0.0001, True)
 print 'Adaptive trapezoidal rule = ' + str(ans2)
+
+ans3 = simpson(exp, a, b, n2)
+print 'Simpson rule = ' + str(ans3)
